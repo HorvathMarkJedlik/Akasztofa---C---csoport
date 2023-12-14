a031163df4c2eeab422247b4704191639ade5c7f
lajosbarnabas <lajos.barnabas@students.jedlik.eu>
2023-12-14 08:44:08 +0100

    Befejeztem a függvényt

--- akasztofa.py
+++ akasztofa.py
@@ -15,4 +15,32 @@ def szo_ki_iro(szo, kitalalt_betuk):
             kimutatás += "_"
     return kimutatás
 
+def akasztófa():
+    random_szo = szo_valaszto()
+    kitalalt_betuk = []
+    probalkozasok_szama = 6
 
+    print("Üdvözöllek az Akasztófa játékban!")
+    print(szo_ki_iro(random_szo, kitalalt_betuk))
+
+    while probalkozasok_szama > 0:
+        tipp = input("Adj meg egy betűt: ").lower()
+
+        kitalalt_betuk.append(tipp)
+
+        if tipp not in random_szo:
+            probalkozasok_szama -= 1
+            print(f"Hibás tipp! {probalkozasok_szama} próbálkozásod maradt.")
+        else:
+            print("Helyes tipp!")
+            print(f"{probalkozasok_szama} próbálkozásod van még.")
+
+        aktualis_allapot = szo_ki_iro(random_szo, kitalalt_betuk)
+        print(aktualis_allapot)
+
+        if "_" not in aktualis_allapot:
+            print("Gratulálok, kitaláltad a szót!")
+            break
+
+    if probalkozasok_szama == 0:
+        print(f"Sajnálom, vesztettél. A szó: {random_szo}")
