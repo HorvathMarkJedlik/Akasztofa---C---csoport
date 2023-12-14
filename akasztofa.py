b7fbe2b68c47019256d91842b6f403a5b7f6f150
Mark Horvath <horvath.mark1@students.jedlik.eu>
2023-12-14 08:23:22 +0100

    elkészítettem a szó kiválasztó függvényt.

--- akasztofa.py
+++ akasztofa.py
@@ -1,21 +1,8 @@
-from random import choice
+import random
 
-szavak: list = []
-
-def beolvas(filename):
-    file = open(filename, 'r', encoding='UTF8')
-    for sor in file:
-        szavak.append(sor.strip())
-    file.close()
-
-
-def randomszo(szavak: list):
-   szo = choice(szavak)
-   return szo
-
-def bontas(szo: str):
-    for betu in szo:
-        print('_', end=' ')
-          
+def szo_valaszto():
+    with open("szavak.txt", "r", encoding="utf-8") as file:
+        szavak = file.read().splitlines()
+    return random.choice(szavak)
 
 
